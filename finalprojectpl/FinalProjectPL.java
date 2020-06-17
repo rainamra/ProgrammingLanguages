1640d794acd85cdfa31f2623f6b7f97ed6e33733
rainamra <56628802+rainamra@users.noreply.github.com>
2020-06-17 21:32:07 +0700

    Add files via upload

---
+++ finalprojectpl/FinalProjectPL.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectpl;
+
+/**
+ *
+ * @author rainamra
+ */
+public class FinalProjectPL {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WelcomePage wp = new WelcomePage();
+        wp.setLocationRelativeTo(null);
+        wp.setVisible(true);
+        
+    }
+    
+}
---
+++ finalprojectpl/otherMethods.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectpl;
+/**
+ *
+ * @author rainamra
+ */
+public interface otherMethods {
+    //Show message dialog box
+    public void infoMessage(String message, String tittle);
+    //Clear the textFields
+    public void clearFieldValue();
+}
